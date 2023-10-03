059f87e26b539fbeee360ecd344370d7302c3bb6
82102 <tigertjdgh@gmail.com>
2023-10-03 17:40:57 +0900

    상담 엔티티 추가

--- src/main/java/com/ksh/loan/domain/BaseEntity.java
+++ src/main/java/com/ksh/loan/domain/BaseEntity.java
@@ -17,15 +17,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @MappedSuperclass
-@EntityListeners((AuditingEntityListener.class))
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
 
     @CreatedDate
-    @Column(updatable = false, columnDefinition = "dataetime default CURRENT_TIMESTAMP NOT NULL COMMENT '생성일자'")
+    @Column(updatable = false, columnDefinition = "datetime default CURRENT_TIMESTAMP NOT NULL COMMENT '생성일자'")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP ON UPPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '수정일자'")
+    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '수정일자'")
     private LocalDateTime updatedAt;
 
     @Column(columnDefinition = "bit default false NOT NULL COMMENT '이용가능여부'")
--- src/main/java/com/ksh/loan/domain/Counsel.java
+++ src/main/java/com/ksh/loan/domain/Counsel.java
@@ -23,27 +23,27 @@ public class Counsel extends BaseEntity{
     @Column(nullable = false, updatable = false)
     private Long counselId;
 
-    @Column
+    @Column(columnDefinition = "datetime DEFAULT NULL COMMENT '신청일자'")
     private LocalDateTime appliedAt;
 
-    @Column
+    @Column(columnDefinition = "varchar(12) DEFAULT NULL COMMENT '상담 요청자'")
     private String name;
 
-    @Column
+    @Column(columnDefinition = "varchar(13) DEFAULT NULL COMMENT '전화번호'")
     private String cellPhone;
 
-    @Column
+    @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '책임자 이메일'")
     private String email;
 
-    @Column
+    @Column(columnDefinition = "text DEFAULT NULL COMMENT '상담 메모'")
     private String memo;
 
-    @Column
+    @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소'")
     private String address;
 
-    @Column
+    @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소 상세'")
     private String addressDetail;
 
-    @Column
+    @Column(columnDefinition = "varchar(5) DEFAULT NULL COMMENT '우편번호'")
     private String zipCode;
 }
