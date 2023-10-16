928533e9588d8a994143c3d233b9fa36cb241d9e
kimsungho <tigertjdgh@gmail.com>
2023-10-16 23:09:53 +0900

    feat: 대출 신청 서류 전체 삭제 기능 구현

--- src/main/java/com/ksh/loan/controller/ApplicationController.java
+++ src/main/java/com/ksh/loan/controller/ApplicationController.java
@@ -82,4 +82,10 @@ public class ApplicationController extends AbstractController {
 
         return ok(fileInfos);
     }
+
+    @DeleteMapping("/files")
+    public ResponseDTO<Void> deleteAll() {
+        fileStorageService.deleteAll();;
+        return ok();
+    }
 }
